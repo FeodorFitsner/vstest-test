e764c8e7d695fa55666095292325b7f22ccf9823
Feodor Fitsner <feodor@fitsner.com>
2015-06-21 09:56:19 -0700

    First checkin

---
+++ vstest-tests/vstest-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace vstest_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = 4;
+            Assert.AreEqual(4, a);
+        }
+    }
+}
